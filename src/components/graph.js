cda65b83a669c13eb52b83f61dd78ec8c6205127
Dillan Teagle <dillan.teagle.va@gmail.com>
2020-05-30 10:23:52 -0500

    :bento: add gh logo and user/repo

--- src/components/graph.js
+++ src/components/graph.js
@@ -3,9 +3,6 @@ import styled from "styled-components";
 import data from "../../colors.js";
 
 
-
-
-
 const LanguageWrapper = (props) => {
   const color = Object.entries(data).filter(key => key[0] == props[0])
   const hex = color[0][1]
